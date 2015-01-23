73b194bef1945b9c7346f186a6856363f5ba5329
rowland rudolf <rowlandrudolf@rowlands-MacBook-Pro.local>
2015-01-22 23:13:12 -0500

    Added auth_token to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          #omniauthable,:omnitauth_proivder => [:facebook]
- 
+  
   has_one :profile, dependent: :destroy 
   accepts_nested_attributes_for :profile
   #delegate :full_name, :current_location, :gender, :age, to: :profile 
 
+  validates_uniqueness_of :auth_token 
   validates_presence_of :profile 
 
-
-  # token auth
+  before_create :set_auth_token, unless: ->(u){ u.auth_token.present? } #before_validation to sign up with facebook?
 
   # has_many :spots
   # has_many :events
@@ -20,4 +20,21 @@ class User < ActiveRecord::Base
   # has_many :favorite_spots, through: :favorites, source: :spot
 
   # has_many :checkins, dependent: :destroy
+
+  #omniauth 
+
+
+private
+  
+  def set_auth_token
+  	self.auth_token = generate_auth_token
+  end
+
+  def generate_auth_token
+  	loop do
+      token = Devise.friendly_token
+  	  break token unless self.class.exists?(auth_token: token)
+    end 
+  end
+
 end
